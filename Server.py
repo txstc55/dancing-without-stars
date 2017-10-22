69da3178b7c5732bf63f3b3cc1b36f7af9f1c04a
Taikun Guo <billg1990@gmail.com>
2017-10-22 16:19:24 -0400

    Modified server.

--- Server.py
+++ Server.py
@@ -1,10 +1,11 @@
-from socket import socket
+import socket
+
 
 class Server(object):
     
   def __init__(self, host, port):
     """Init the server, set two sockets for players"""
-    self.socket = socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     self.socket.bind((host, port))
     self.sockets = [None, None]
@@ -14,18 +15,22 @@ class Server(object):
     """Waiting for two players to connect."""
     print("Waiting for Choreographer...")
     self.sockets[0], _ = self.socket.accept()
-    print("Connection from Choreographer established.")
+    choreographer = str(self.receive(0))
+    print("Connection from " + choreographer + " as Choreographer established.")
     print("Waiting for Spoiler...")
     self.sockets[1], _ = self.socket.accept()
-    print("Connection from Spoiler established.")
+    spoiler = str(self.receive(1))
+    print("Connection from " + spoiler + " as Spoiler established.")
+    return choreographer, spoiler
 
   def send_all(self, data):
     """send data to both players"""
-    s.sendall(data) for s in self.sockets
+    for socket in self.sockets:
+      socket.sendall(bytes(data, "utf-8"))
 
   def receive(self, player):
     """receive data from one player"""
-    return self.sockets[player].recv(4096)
+    return self.sockets[player].recv(4096).decode("utf-8")
 
   def __del__(self):
     self.socket.close()
