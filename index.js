52df3dd3e78325a3fa9d906a1ce65e92adbb630f
Taikun Guo <billg1990@gmail.com>
2017-10-26 15:23:58 -0400

    Added function to update state.

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http)
 
-let STATE = ""
+let STATE = null; // the current state of the game
+
+// when someone connected, send out the current state
+io.on('connection', (socket) => {
+  console.log('A connection ' + socket.id + ' established.');
+  // send the latest state to the new connector
+  io.emit('STATE', { board_size : 40 });
+  socket.on('disconnect', () => {
+    console.log('Connection ' + socket.id + ' closed.');
+  });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('A connection ' + socket.id + ' established.');
-  io.emit('STATE', { data : [1, 2, 3, 4, 5, 6] });
-  socket.on('disconnect', () => {
-    console.log('Connection ' + socket.id + ' closed.');
-  });
+app.post('/', (req, res, body) => {
+
 });
 
 http.listen(3000, () => {
