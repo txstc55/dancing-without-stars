ad304cd65cffdc49d47e7e83ee89cfe611ae4e13
txstc55 <txstc55@gmail.com>
2018-10-25 21:34:56 -0400

    change from space to tab

--- ui.py
+++ ui.py
@@ -11,15 +11,15 @@ import requests
 #  board: [[]]
 #}
 def update_state(board_size, num_color, chore, spoi, stage, board, timer):
-  data = { \
-    "board_size" : board_size, \
-    "num_color" : num_color, \
-    "choreographer" : chore, \
-    "spoiler" : spoi, \
-    "stage" : stage, \
-    "board" : board, \
-    "timer" : timer }
-  try:
-    r = requests.post("http://127.0.0.1:3000/", json=data)
-  except RuntimeError:
-    print("[ERROR] failed to update state to frontends!!!")
+	data = { \
+		"board_size" : board_size, \
+		"num_color" : num_color, \
+		"choreographer" : chore, \
+		"spoiler" : spoi, \
+		"stage" : stage, \
+		"board" : board, \
+		"timer" : timer }
+	try:
+		r = requests.post("http://127.0.0.1:3000/", json=data)
+	except RuntimeError:
+		print("[ERROR] failed to update state to frontends!!!")
